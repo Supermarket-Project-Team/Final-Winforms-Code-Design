6d143a5aaeec82982da1e82ad3d359e2e7e5e837
Sukritha <sukritha7@gmail.com>
2020-09-11 13:14:25 +0530

    Login page valiadtion half completed

--- Login_Form/Login_Form/Form1.cs
+++ Login_Form/Login_Form/Form1.cs
@@ -7,24 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Login_Form
 {
-    public partial class Form1 : Form
+    public partial class form_login : Form
     {
-        public Form1()
+        SqlConnection con;
+        SqlDataAdapter adp;
+        DataTable dt;
+        public form_login()
         {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-
+            con = new SqlConnection("data source=KRIPARAJ\\SQLEXPRESS; database=SuperMarket; user ID=sa; password=Sukritha7###");
+            con.Open();
         }
-
+        
         private void btn_Login_Click(object sender, EventArgs e)
         {
+            string query = "SELECT * FROM USER_TABLE WHERE USER_NAME= '" + txt_username.Text.Trim() + "' and PASSWORD='" + txt_password.Text.Trim() + "' ";
+            adp = new SqlDataAdapter(query, con);
+            dt = new DataTable();
+            adp.Fill(dt);
+            if (dt.Rows.Count == 1)
+            {
+                AdminLanding objadminlanding = new AdminLanding();
+                this.Hide();
+                objadminlanding.Show();
+                MessageBox.Show("Done");
+            }
+            else
+            {
+                MessageBox.Show("Username or password is incorrect");
+            }
+        }
 
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
